26290d0c53742a368e317a45396a495f2997f2b0
Nicholas D. Socci <soccin@mskcc.org>
2015-09-06 18:11:21 -0400

    Fixed bug in fill2maf for complex in/del
    The script to convert the fillout fill back to MAF
    was failing for complex insertions and deletions due to
    bugs in the vcf 2 maf event translation function
    (vcf2mafEvent)

--- zeng2MAFFill/__main__.py
+++ zeng2MAFFill/__main__.py
@@ -56,17 +56,14 @@ def vcf2mafEvent(chrom,pos,ref,alt):
         startPos=pos
     elif delta>0:
         endPos=str(int(pos)+len(refN)-1)
-        startPos=str(int(pos)+1)
-        refN=refN[1:]
-        if len(altN)==1:
-            altN='-'
-        else:
-            altN=altN[1:]
+        startPos=str(int(pos)+len(refN)-delta)
+        refN=refN[(len(refN)-delta):]
+        altN='-'
     else:
         endPos=str(int(pos)+1)
         startPos=pos
         refN="-"
-        altN=altN[1:]
+        altN=altN[len(ref):]
     return (chrom,startPos,endPos,refN,altN)
 
 for rec in cin:
