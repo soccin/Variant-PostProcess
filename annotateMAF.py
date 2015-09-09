02dbf49bbd483cbc00c3e383cee741ae8da50f38
Nicholas D. Socci <soccin@mskcc.org>
2015-09-09 11:49:41 -0400

    Fixed missing remote and added uncommited tag
    Fixed problem if repo is only local and added an untracked tag with datetime stamp

--- annotateMAF.py
+++ annotateMAF.py
@@ -1,6 +1,8 @@
+#
 import sh
 import os.path
 import sys
+import datetime
 
 def printAnnotation(fp=sys.stdout):
     SDIR=os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -8,10 +10,19 @@ def printAnnotation(fp=sys.stdout):
     WORKTREE="--work-tree=%s" % (SDIR)
     VERSIONBRANCH=sh.git(GITDIR,WORKTREE,"describe","--all").strip()
     VERSIONTAG=sh.git(GITDIR,WORKTREE,"describe","--long","--always").strip()
-    REMOTE_ORIGIN_URL=sh.git(GITDIR,WORKTREE,"config","--get","remote.origin.url").strip()
+    try:
+        REMOTE_ORIGIN_URL=sh.git(GITDIR,WORKTREE,"config","--get","remote.origin.url").strip()
+    except:
+        REMOTE_ORIGIN_URL=SDIR
 
+    UNTRACKED=sh.git(GITDIR,WORKTREE,"status","--porcelain").strip()
+    if UNTRACKED!="":
+        UNTRACKED=" [UnCommitted Changes %s]" % (str(datetime.datetime.today()))
 
-    print >>fp, "#CBE:%s (%s) (%s)" % (REMOTE_ORIGIN_URL,VERSIONBRANCH,VERSIONTAG)
+    print >>fp, "#CBE:%s (%s) (%s)%s" % (REMOTE_ORIGIN_URL,VERSIONBRANCH,VERSIONTAG,UNTRACKED)
     print >>fp, "#CBE:%s %s" % (os.path.realpath(sys.argv[0]),
                           " ".join(sys.argv[1:]))
+
+if __name__=="__main__":
+    printAnnotation()
 
