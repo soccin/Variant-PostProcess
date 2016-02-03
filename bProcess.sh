c3cd7a6e231b1ff4a6e578e727f3b7afb5d000ec
Nicholas D. Socci <soccin@mskcc.org>
2016-02-03 00:08:30 -0500

    Added explict arg for PROJECTDIR when needed

--- bProcess.sh
+++ bProcess.sh
@@ -4,8 +4,8 @@
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ $# -ne 1 ]; then
-	echo "usage: postProcess.sh PipelineOutputDir"
+if [ $# -lt 1 ]; then
+	echo "usage: postProcess.sh PipelineOutputDir [PROJECTDIR]"
 	exit
 fi
 
@@ -13,22 +13,29 @@ PIPELINEDIR=$1
 
 projectNo=$(echo $PIPELINEDIR | perl -ne 'm|/Proj_([^/\s]*)|; print $1')
 
-NUMDIRS=$(find /ifs/projects/BIC /ifs/projects/CMO -type d | egrep -v "(drafts|archive)" | egrep "Proj_$projectNo$" | wc -l)
-PROJECTDIR=$(find /ifs/projects/BIC /ifs/projects/CMO -type d | egrep -v "(drafts|archive)" | egrep "Proj_$projectNo$")
+if [ $# -eq 1 ]; then
 
-SCRIPT=$(basename $0)
-if [ "$NUMDIRS" != "1" ]; then
-    echo $SCRIPT :: Problem finding project files for Proj_$projectNo
-    echo $SCRIPT NUMDIRS=$NUMDIRS
-    echo
-    exit
+	NUMDIRS=$(find /ifs/projects/BIC /ifs/projects/CMO -type d | egrep -v "(drafts|archive)" | egrep "Proj_$projectNo$" | wc -l)
+	PROJECTDIR=$(find /ifs/projects/BIC /ifs/projects/CMO -type d | egrep -v "(drafts|archive)" | egrep "Proj_$projectNo$")
+	SCRIPT=$(basename $0)
+	if [ "$NUMDIRS" != "1" ]; then
+	    echo $SCRIPT :: Problem finding project files for Proj_$projectNo
+	    echo $SCRIPT NUMDIRS=$NUMDIRS
+	    echo
+	    exit
+	fi
+
+else
+
+	PROJECTDIR=$2
+
 fi
 
 echo $SCRIPT PROJECTDIR=\"$PROJECTDIR\"
-
 echo PIPELINEDIR=$PIPELINEDIR
 echo pairingFile=$PROJECTDIR/*_sample_pairing.txt
 echo projectNo=$projectNo
+exit
 
 
 bsub -o LSF.POST/ -J POST_$projectNo -n 12 -We 59 -R "rusage[mem=64]" \
