991c1c843e20e3feb2df3e1c0d6b85e50d5df25e
Nicholas D. Socci <soccin@mskcc.org>
2016-03-29 19:52:48 -0400

    Update to latest vcf2maf (1.6.6) and vep (83)

--- paths.sh
+++ paths.sh
@@ -1,6 +1,6 @@
 VARIANTSPIPEDIR=/home/socci/Code/Pipelines/CBE/Variant/variants_pipeline
 BEDTOOLS=/opt/common/CentOS_6/bedtools/bedtools-2.22.0/bin/bedtools
-VEPPATH=/opt/common/CentOS_6/vep/v82
+VEPPATH=/opt/common/CentOS_6/vep/v83
 PERL=/opt/common/CentOS_6-dev/perl/perl-5.22.0/bin/perl
-VCF2MAF=/opt/common/CentOS_6/vcf2maf/v1.6.2
+VCF2MAF=/opt/common/CentOS_6/vcf2maf/v1.6.6
 MSK_ISOFORMS=$VCF2MAF/data/isoform_overrides_at_mskcc
