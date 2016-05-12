b9836158c7c5ef53de0ec3cdf6bd71344812107b
Nicholas D. Socci <soccin@mskcc.org>
2016-05-11 21:14:10 -0400

    change delivery to rsync

--- deliverResults.sh
+++ deliverResults.sh
@@ -4,6 +4,6 @@
 
 ln -s $PIPELINEDIR/variants/copyNumber/facets $PIPELINEDIR
 mkdir $PIPELINEDIR/post
-cp Proj_*___SOMATIC_FACETS.vep.filtered.maf $PIPELINEDIR/post
-cp ___FILLOUT.maf $PIPELINEDIR/post/Proj_${projectNo}___FILLOUT.maf
+rsync -avP Proj_*___SOMATIC_FACETS.vep.filtered.maf $PIPELINEDIR/post
+rsync -avP ___FILLOUT.maf $PIPELINEDIR/post/Proj_${projectNo}___FILLOUT.maf
 
