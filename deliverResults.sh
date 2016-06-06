a91e32325531f898e7ecc3ef5e4ddf3fb3a4c1ac
Nicholas D. Socci <soccin@mskcc.org>
2016-06-06 18:35:44 -0400

    removed facets link

--- deliverResults.sh
+++ deliverResults.sh
@@ -2,7 +2,7 @@
 
 . ../config
 
-ln -s $PIPELINEDIR/variants/copyNumber/facets $PIPELINEDIR
+#ln -s $PIPELINEDIR/variants/copyNumber/facets $PIPELINEDIR
 mkdir $PIPELINEDIR/post
 rsync -avP Proj_*___SOMATIC_FACETS.vep.filtered.maf $PIPELINEDIR/post
 rsync -avP ___FILLOUT.maf $PIPELINEDIR/post/Proj_${projectNo}___FILLOUT.maf
