4a1ddce222c3c35b14d5c7ded7074d86262d5d8c
Nicholas D. Socci <soccin@mskcc.org>
2016-12-10 16:41:55 -0500

    First part of getMergeMAF working

--- getMergedMAF.sh
+++ getMergedMAF.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
+BICPIPEDIR=/ifs/work/socci/Pipelines/CBE/variants_pipeline
 
-PIPELINEDIR=$1
+if [ "$#" != "3" ]; then
+    echo "usage: getMergedMAF.sh PROJECTNO PIPELINEDIR PAIRINGFILE [GENOME]"
+    exit
+fi
 
+PROJECTNO=$1
+PIPELINEDIR=$2
+PAIRINGFILE=$3
 
-#./FixMultiInDel/fixMultiInDel.sh /ifs/res/seq/thompsonc/intlekoa/Proj_06265_EO/r_001/variants/snpsIndels/haplotypecaller/Proj_06265_EO_HaplotypeCaller.vcf
+if [ "$#" == "4" ]; then
+    GENOME=$4
+else
+    GENOME=b37
+fi
 
-/ifs/work/socci/Pipelines/CBE/variants_pipeline/haploTect_merge.pl \
-    -pair /ifs/projects/BIC/variant/Proj_06265_EO/Proj_06265_EO_sample_pairing.txt \
-    -hc_vcf Proj_06265_EO_HaplotypeCaller___FixInDels.vcf \
-    -mutect_dir /ifs/res/seq/thompsonc/intlekoa/Proj_06265_EO/r_001/variants/snpsIndels/mutect \
-    -pre Proj_nds \
-    -output /home/socci/Code/Pipelines/CBE/Variant/CallSomaticIndelHaplotype/output \
-    -species b37 \
-    -config /ifs/work/socci/Pipelines/CBE/variants_pipeline/variants_pipeline_config.txt \
+ODIR=_mergedMAF
+mkdir -p $ODIR
+
+echo -n "fixing In/Dels..."
+$SDIR/FixMultiInDel/fixMultiInDel.sh \
+    $PIPELINEDIR/variants/snpsIndels/haplotypecaller/${PROJECTNO}_HaplotypeCaller.vcf \
+    $ODIR/___FixInDels.vcf
+
+echo "done"
+
+exit
+
+$BICPIPEDIR/haploTect_merge.pl \
+    -config $BICPIPEDIR/variants_pipeline_config.txt \
+    -pre $PROJECTNO \
+    -pair $PAIRINGFILE \
+    -species $GENOME \
     -exac_vcf /opt/common/CentOS_6/vep/v86/ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz
+    -output $ODIR \
+    -mutect_dir $PIPELINEDIR/variants/snpsIndels/mutect \
+    -hc_vcf $ODIR/___FixInDels.vcf
