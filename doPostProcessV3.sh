9f43904fcba78f51a3947be9437eefbf1db39c1d
Nicholas D. Socci <soccin@mskcc.org>
2016-12-13 09:56:19 -0500

    Added code to get list of normal samples (from V2)

--- doPostProcessV3.sh
+++ doPostProcessV3.sh
@@ -84,6 +84,42 @@ echo "Applying filter_ffpe_pool"
 $WESFBIN/applyFilter.sh filter_ffpe_pool.R mafA mafB -f ffpePoolFill.out
 echo "Applying filter_normal_panel"
 $WESFBIN/applyFilter.sh filter_normal_panel.R mafB mafC -f normalCohortFill.out
+
+###############################################################
+#
+# See if there is a patient file to identify normal samples
+#
+
+PATIENTFILE=$(ls -d $PROJECTDIR/* | fgrep _sample_patient.txt)
+
+if [ "$PATIENTFILE" != "" ]; then
+    echo "Using PATIENTFILE="$PATIENTFILE
+    cat $PATIENTFILE  | awk -F"\t" '$5=="Normal"{print $2}' >_normalSamples
+else
+    PAIRINGFILE=$(ls -d $PROJECTDIR/* | fgrep _sample_pairing.txt)
+    if [ "$PAIRINGFILE" != "" ]; then
+        echo "WARNING: Can not find PATIENT FILE"
+        echo "PAIRING file used to infer normals; might not be correct"
+        echo "Using PAIRINGFILE="$PAIRINGFILE
+        cat $PAIRINGFILE  | cut -f1 | sort | uniq >_normalSamples
+    else
+        echo "FATAL ERROR: Cannot find PATIENT nor PAIRINGFILE"
+        echo "Can not determine normal samples"
+        exit 1
+    fi
+fi
+
+NUM_NORMALS=$(wc -l _normalSamples | awk '{print $1}')
+if [ "$NUM_NORMALS" == "0" ]; then
+    echo
+    echo "FATAL ERROR: Found 0 Normals; if this is really true implement override"
+    echo
+    exit 1
+fi
+
+# Finished getting normal samples
+###############################################################
+
 echo "Applying filter_cohort_normals"
 $WESFBIN/applyFilter.sh filter_cohort_normals.R mafC mafD -f ___FILLOUT.maf -N _normalSamples
 
