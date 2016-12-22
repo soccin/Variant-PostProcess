f0b9137cce5987fabb7f75afe65a290971680539
Nicholas D. Socci <soccin@mskcc.org>
2016-12-21 20:57:07 -0500

    Updated VEP/VCF2MAF paths to latest

--- vcf2MultiMAF_b37.sh
+++ vcf2MultiMAF_b37.sh
@@ -2,13 +2,12 @@
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-VARIANTSPIPEDIR=/home/socci/Code/Pipelines/CBE/Variant/variants_pipeline
 BEDTOOLS=/opt/common/CentOS_6/bedtools/bedtools-2.22.0/bin/bedtools
-VEPPATH=/opt/common/CentOS_6/vep/v83
-#VEPPATH=/scratch/socci/v83
 PERL=/opt/common/CentOS_6-dev/perl/perl-5.22.0/bin/perl
 PYTHON=/opt/common/CentOS_6-dev/bin/current/python
-VCF2MAF=/opt/common/CentOS_6/vcf2maf/v1.6.6
+
+VEPPATH=/opt/common/CentOS_6/vep/v86
+VCF2MAF=/opt/common/CentOS_6/vcf2maf/v1.6.11
 MSK_ISOFORMS=$VCF2MAF/data/isoform_overrides_at_mskcc
 
 ln -s /ifs/depot/assemblies/H.sapiens/b37/b37.fasta
