4fc4d388c5c362eecacf3567536771352b570272
Nicholas D. Socci <soccin@mskcc.org>
2016-12-28 15:11:28 -0500

    Script to get BED file from VCF events

--- getInDelRegionsFromVCF.sh
+++ getInDelRegionsFromVCF.sh
@@ -1,1 +1,23 @@
-cat $HAPMAF | egrep -v "^#" | awk '{print $1,$2-1,$2,length($4),length($5),$4,$5}' | awk '$4>$5{print $1,$2,$3+$4} $5>$4{print $1,$2,$3+$5}' | tr ' ' '\t' | bedtools slop -b 250 -i - -g ~/lib/bedtools/genomes/human.b37.genome  | sort -k1,1V -k2,2n | bedtools merge -i - | awk '{print s+=($3-$2)/1e6}'
+#!/bin/bash
+
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+HAPMAF=$1
+GENOME=$2
+
+PAD=250
+
+#
+# Get all the IN/DEL events from a VCF
+# And convert to a BED file, with some 
+# extra padding around event. 
+#
+
+cat $HAPMAF \
+	| egrep -v "^#" \
+	| awk '{print $1,$2-1,$2,length($4),length($5)}' \
+	| awk '$4>$5{print $1,$2,$3+$4} $5>$4{print $1,$2,$3+$5}' \
+	| tr ' ' '\t' \
+	| bedtools slop -b $PAD -i - -g $GENOME \
+	| sort -k1,1V -k2,2n \
+	| bedtools merge -i -
