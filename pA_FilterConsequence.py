663fa19d1f93a7cb625b021614fc1741a348fb08
Nicholas D. Socci <soccin@mskcc.org>
2017-04-22 16:44:39 -0400

    Change variant types filtered, added in Comment TAG

--- pA_FilterConsequence.py
+++ pA_FilterConsequence.py
@@ -4,20 +4,38 @@ from PAWK import PAWK
 import sys
 
 consequenceDb=set("""
+coding_sequence_variant
 frameshift_variant
 inframe_deletion
 inframe_insertion
 missense_variant
 splice_acceptor_variant
 splice_donor_variant
-splice_region_variant
+start_lost
 stop_gained
+stop_lost
 """.strip().split())
 
+def readWriteHeader(fp,newHeaderLines=[]):
+    for line in fp:
+        if line.startswith("#"):
+            print line,
+        else:
+            break
 
-print sys.stdin.readline(),
-print "#PostProcessV1 Filter Consequence (noUTR)"
-for rec in PAWK(sys.stdin):
+    for hi in newHeaderLines:
+        print hi
+
+    yield line
+    for line in fp:
+        yield line
+
+
+inStream=readWriteHeader(sys.stdin,
+    ["#CBE:PostProcessV2(mouse) Filter Consequence (noUTR)"]
+    )
+
+for rec in PAWK(inStream):
     if rec.Consequence in consequenceDb:
         rec.write()
 
